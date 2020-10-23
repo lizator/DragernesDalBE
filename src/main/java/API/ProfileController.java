5c31100776e7d70de91a2548ff8d09be859cea62
lizator <frederikkoefoeds@gmail.com>
2020-10-23 14:42:15 +0200

    Added Create call and testet with JUnit and postman
    TODO: Setup test db

--- src/main/java/API/ProfileController.java
+++ src/main/java/API/ProfileController.java
@@ -2,18 +2,27 @@ package API;
 
 import dal.ProfileDAO;
 import dal.dto.ProfileDTO;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.WebApplicationException;
 
 @RestController
 public class ProfileController {
     ProfileDAO dao = new ProfileDAO();
 
     @PostMapping(value = "/user/getbyemail", consumes = "application/json", produces = "application/json")
-    public ProfileDTO getProfileByEmail(@RequestBody ProfileDTO dto) {
+    public ProfileDTO getProfileByEmail(@RequestBody ProfileDTO dto) throws WebApplicationException {
         return dao.getProfileByEmail(dto.getEmail());
+    }
+
+    @PostMapping(value = "/user/create", consumes = "application/json", produces = "application/json")
+    public ProfileDTO createUser(@RequestBody ProfileDTO dto) throws WebApplicationException {
+        if (!dao.getEmailExists(dto.getEmail())) {
+            System.out.println("In here");
+            System.out.println(dao.getEmailExists(dto.getEmail()));
+            return dao.createUser(dto);
+        }
+        throw new WebApplicationException("Email already exists in database", 409);
     }
 
 
--- src/test/java/dal/ProfileDAOTest.java
+++ src/test/java/dal/ProfileDAOTest.java
@@ -19,4 +19,26 @@ public class ProfileDAOTest {
         }
         assertEquals(2, testuser.getId());
     }
+
+    @Test
+    public void getEmailExists() {
+        boolean testTrue = dao.getEmailExists("test@gmail.com");
+        boolean testFalse = dao.getEmailExists("notEvenMail.com");
+        assertTrue(testTrue);
+        assertFalse(testFalse);
+    }
+
+    @Test
+    public void getNextID() {
+        int test = dao.getNextID();
+        assertEquals(3, test);
+    }
+
+    @Test
+    public void createUser() {
+        ProfileDTO test = new ProfileDTO(0, "name1", "name2", "test2@gmail.com", 12342413, "nope", "Salt", false);
+        test = dao.createUser(test);
+        assertEquals(3, test.getId());
+        assertEquals("name1", test.getFirstName());
+    }
 }
