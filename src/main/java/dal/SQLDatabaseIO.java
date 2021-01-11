624a875d8f49ef0fe9ad887059502bc5472d4088
Mark <s174881@student.dtu.dk>
2021-01-11 12:03:17 +0100

    Autoreconnect fixed

--- src/main/java/dal/SQLDatabaseIO.java
+++ src/main/java/dal/SQLDatabaseIO.java
@@ -26,7 +26,7 @@ public class SQLDatabaseIO {
     public SQLDatabaseIO(String USER, String PASSWORD, String URL, int PORT) {
         this.USER = USER;
         this.PASS = PASSWORD;
-        this.DatabaseURL = "jdbc:mysql://" + URL + ":"+PORT+"/"+db_name+"?characterEncoding=latin1&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT?autoReconnect=true";
+        this.DatabaseURL = "jdbc:mysql://" + URL + ":"+PORT+"/"+db_name+"?characterEncoding=latin1&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&autoReconnect=true";
     }
 
     /**
