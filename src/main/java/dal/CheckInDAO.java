17451898f88f5cb23468aef94a1cd87575ece997
Mark <s174881@student.dtu.dk>
2021-01-12 11:46:48 +0100

    Tildel 1 EP tilføjet

--- src/main/java/dal/CheckInDAO.java
+++ src/main/java/dal/CheckInDAO.java
@@ -28,4 +28,21 @@ public class CheckInDAO {
         }
         return checkInDTO;
     }
+
+    public CheckInDTO addEP(CheckInDTO dto){
+        try {
+            db.connect();
+            db.update("START TRANSACTION;",new String[]{});
+            db.update("update companiondb.character as t1, companiondb.eventAttendancyList as t2 SET t1.currentep = currentep +1 WHERE t1.idcharacter = t2.idcharacter and t2.checkIn = true and t2.idevent = ?;",
+                    new String[] {dto.getIdEvent()+""});
+            db.update("COMMIT", new String[]{});
+            db.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error in DB with character");
+            //throw new SQLException("Error in Database");
+        }
+        return dto;
+    }
 }
